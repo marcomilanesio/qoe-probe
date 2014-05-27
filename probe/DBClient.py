d5e13f0515441226bec1c38ef7974f210269aa02
salvo1404 <salvatorebalzano@hotmail.it>
2014-05-27 17:23:05 +0200

    fields elimination in DB interface

--- probe/DBClient.py
+++ probe/DBClient.py
@@ -47,12 +47,12 @@ class DBClient:
 
     def create_idtable(self):
 	cursor = self.conn.cursor()	
-	cursor.execute('''CREATE TABLE IF NOT EXISTS client_id (id INT4, first_start TEXT)''')	
+	cursor.execute('''CREATE TABLE IF NOT EXISTS client_id (probe_id INT4, first_start TEXT)''')	
 	self.conn.commit()
 
     def get_clientID(self):
 	client_id = 0	
-	query = "SELECT id FROM client_id"
+	query = "SELECT probe_id FROM client_id"
         res = self.execute_query(query)
 	if res != []:
             client_id = int(res[0][0])
@@ -73,15 +73,15 @@ class DBClient:
     def create_plugin_table(self):
         #create a Table for the Firefox plugin
         cursor = self.conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS %s (log_reason TEXT, ff_version TEXT, method TEXT, host TEXT, uri TEXT, 
-        request_event_ts TIMESTAMP, content_type TEXT, content_length INT4, AcceptEncoding TEXT, ContentEncoding TEXT, 
-        server_cnxs TEXT, server_http TEXT, http_id INT8, session_start TIMESTAMP, session_url TEXT, if_complete_cache INT4, 
-        localAddress INET, localPort INT4, remoteAddress INET, remotePort INT4, response_code INT4, http_request_bytes INT4, 
-        http_header_bytes INT4, http_body_bytes INT4, http_cache_bytes INT4, dns_start TIMESTAMP, dns_time INT4, syn_start TIMESTAMP, 
-        tcp_cnxting INT4, send_ts TIMESTAMP, send INT4, GET_sent TIMESTAMP, First_Bytes TIMESTAMP, app_rtt INT4, EndTime TIMESTAMP, 
-        data_trans INT4, full_load_time INT4, content_load_time INT4, tabId INT8, current_wifi_quality TEXT, cpu_idle TEXT, 
-        cpu_percent_ffx TEXT, mem_free TEXT, mem_used TEXT, mem_percent_ffx TEXT, ping_gw TEXT, ping_dns TEXT, ping_google TEXT, 
-        nr_annoying INT4, location TEXT, obj_aborted INT4, clientID INT8, cmt TEXT, sid INT8)''' % self.dbconfig['rawtable']) 
+        cursor.execute('''CREATE TABLE IF NOT EXISTS %s (host TEXT, uri TEXT, 
+        request_ts TIMESTAMP, content_type TEXT, content_len INT4,  
+        keep_alive TEXT, httpid INT8, session_start TIMESTAMP, session_url TEXT, cache INT4, 
+        local_ip INET, local_port INT4, remote_ip INET, remote_port INT4, response_code INT4, get_bytes INT4, 
+        header_bytes INT4, body_bytes INT4, cache_bytes INT4, dns_start TIMESTAMP, dns_time INT4, syn_start TIMESTAMP, 
+        syn_time INT4, is_sent INT4, get_sent_ts TIMESTAMP, first_bytes_rcv TIMESTAMP, app_rtt INT4, end_time TIMESTAMP, 
+        rcv_time INT4, full_load_time INT4, tab_id INT8,  
+        cpu_percent TEXT, mem_percent TEXT, ping_gateway TEXT, ping_google TEXT, 
+        annoy INT4, probe_id INT8, sid INT8)''' % self.dbconfig['rawtable']) 
         self.conn.commit()
 
     def create_activemeasurement_table(self):
@@ -94,21 +94,21 @@ class DBClient:
         #insert a directory into the db
         cursor = self.conn.cursor()
         for obj in datalist:
-	    if obj.has_key("pageURL"):
-	    	mem_perc = stats[str(obj["pageURL"])]['mem']
-                cpu_perc = stats[str(obj["pageURL"])]['cpu']
-             	state = '''INSERT INTO %s VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s', '%s', '%s', '%s', %d, '%s', 
-            	'%s', %d, '%s', %d, '%s', %d, %d, %d, %d, %d, %d, '%s', %d, '%s', %d, '%s', %d, '%s', '%s', %d, '%s', %d,
-                    %d, %d, %d, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%s', %d, %d, '%s')
-                    ''' % (self.dbconfig['rawtable'], str(obj["log"]), str(obj["ff_v"]), str(obj["method"]), str(obj["host"]),\
-                     str(obj["uri"]), str(obj["ts"]), str(obj["type"]), int(obj["len"]), str(obj["C_Encode"]), str(obj["Encode"]), \
-                     str(obj["s_cnxs"]), str(obj["s_http"]), int(obj["httpid"]), str(obj["pageStart"]), str(obj["pageURL"]), int(obj["cache"]), \
-                     str(obj["cIP"]), int(obj["cPort"]), str(obj["sIP"]), int(obj["sPort"]), int(obj["status"]), int(obj["GET_Byte"]), \
-                     int(obj["HeaderByte"]), int(obj["BodyByte"]), int(obj["CacheByte"]), str(obj["dns1"]), int(obj["dns"]), str(obj["tcp1"]), \
-                     int(obj["tcp"]), str(obj["sendTS"]), int(obj["send"]), str(obj["http1"]), str(obj["http2"]), int(obj["http"]), str(obj["EndTS"]), \
-                     int(obj["rcv"]), int(obj["onLoad"]), int(obj["onContent"]), int(obj["tabId"]), str(obj["wifi"]), str(obj["CPUidle"]), \
-                     str(cpu_perc), str(obj["MEMfree"]), str(obj["MEMused"]), str(mem_perc), str(obj["pingGW"]), str(obj["pingDNS"]), \
-                     str(obj["pingG"]), int(obj["AnnoyNr"]), str(obj["location"]), int(obj["IfAborted"]), int(obj["ID"]), str(obj["cmt"]))
+	    if obj.has_key("session_url"):
+	    	mem_perc = stats[str(obj["session_url"])]['mem']
+                cpu_perc = stats[str(obj["session_url"])]['cpu']
+             	state = '''INSERT INTO %s VALUES ('%s', '%s', '%s', '%s', %d, '%s', %d, '%s', 
+            	'%s', %d, '%s', %d, '%s', %d, %d, %d, %d, %d, %d, '%s', %d, '%s', %d, %d, '%s', '%s', %d, '%s', %d,
+                    %d, %d, '%s', '%s', '%s', '%s', %d, %d)
+                    ''' % (self.dbconfig['rawtable'], str(obj["host"]),\
+                     str(obj["uri"]), str(obj["request_ts"]), str(obj["content_type"]), int(obj["content_len"]), \
+                     str(obj["keep_alive"]), int(obj["httpid"]), str(obj["session_start"]), str(obj["session_url"]), int(obj["cache"]), \
+                     str(obj["local_ip"]), int(obj["local_port"]), str(obj["remote_ip"]), int(obj["remote_port"]), int(obj["response_code"]), int(obj["get_bytes"]), \
+                     int(obj["header_bytes"]), int(obj["body_bytes"]), int(obj["cache_bytes"]), str(obj["dns_start"]), int(obj["dns_time"]), str(obj["syn_start"]), \
+                     int(obj["syn_time"]), int(obj["is_sent"]), str(obj["get_sent_ts"]), str(obj["first_bytes_rcv"]), int(obj["app_rtt"]), str(obj["end_time"]), \
+                     int(obj["rcv_time"]), int(obj["full_load_time"]), int(obj["tab_id"]), \
+                     str(cpu_perc), str(mem_perc), str(obj["ping_gateway"]), \
+                     str(obj["ping_google"]), int(obj["annoy"]), int(obj["probe_id"]))
 	    	cursor.execute(state)
             	self.conn.commit()
         sid_inserted = self._generate_sid_on_table()        
@@ -144,14 +144,14 @@ class DBClient:
 
     def _generate_sid_on_table(self):
         max_sid = self._select_max_sid()
-        query = "select distinct on (clientID, session_start) clientID, session_start from %s where sid is NULL order by session_start" % self.dbconfig['rawtable']
+        query = "select distinct on (probe_id, session_start) probe_id, session_start from %s where sid is NULL order by session_start" % self.dbconfig['rawtable']
         res = self.execute_query(query)
         logger.debug('Found %d sessions to insert', len(res))
         for i in range(len(res)):
             clientid = res[i][0]
             session_start = res[i][1]
             max_sid += 1
-            query = "update %s set sid = %d where session_start = \'%s\' and clientID = \'%s\'" % (self.dbconfig['rawtable'], max_sid, session_start, clientid)
+            query = "update %s set sid = %d where session_start = \'%s\' and probe_id = \'%s\'" % (self.dbconfig['rawtable'], max_sid, session_start, clientid)
             self.execute_update(query)
         return max_sid
 
@@ -179,7 +179,7 @@ class DBClient:
 
     def force_update_full_load_time(self, sid):
         import datetime
-        q = '''select session_start, endtime from %s where sid = %d''' % (self.dbconfig['rawtable'], sid)
+        q = '''select session_start, end_time from %s where sid = %d''' % (self.dbconfig['rawtable'], sid)
         res = self.execute_query(q)
         session_start = list(set([x[0] for x in res]))[0]
         end_time = max(list(set([x[1] for x in res])))
