233bdfaf7a853fcedd001b2f68a22380ba5bd488
Marco Milanesio <milanesio.marco@gmail.com>
2014-06-16 16:56:19 +0200

    New active measurement monitor.

--- probe/ActiveMeasurement.py
+++ probe/ActiveMeasurement.py
@@ -21,15 +21,23 @@
 import subprocess
 import json
 import re
+import logging
+from DBClient import DBClient
+
+logger = logging.getLogger('Active')
 
 
 class Measure(object):
     def __init__(self, host):
         self.target = host
         self.result = {}
+        self.cmd = ''
 
     def get_result(self):
         return self.result
+
+    def get_cmd(self):
+        return self.cmd
 
 
 class Ping(Measure):
@@ -38,19 +46,15 @@ class Ping(Measure):
         self.cmd = 'ping -c 3 %s ' % self.target
 
     def run(self):
-        #logger.info('pinging %s' % self.target )
-        print 'pinging %s ' % self.target
         ping = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         out, error = ping.communicate()
         res = out.strip().split('\n')[-1].split(' = ')[1].split()[0]
         rttmin = rttavg = rttmax = rttmdev = -1.0
         try:
             rttmin, rttavg, rttmax, rttmdev = map(float, res.strip().split("/"))
-            #logger.info('rtts - %.3f, %.3f, %.3f, %.3f' % (rttmin, rttavg, rttmax, rttmdev) )
-            #print rttmin, rttavg, rttmax, rttmdev
+            logger.info('rtts - %.3f, %.3f, %.3f, %.3f' % (rttmin, rttavg, rttmax, rttmdev) )
         except ValueError:
-            #logger.error('Unable to map float in do_ping [%s]' % out.strip())
-            print '''logger.error('Unable to map float in do_ping [%s]' % out.strip())'''
+            logger.error('Unable to map float in do_ping [%s]' % out.strip())
         self.result = json.dumps({'min': rttmin, 'max':rttmax, 'avg':rttavg, 'std':rttmdev})
 
 
@@ -62,13 +66,12 @@ class Traceroute(Measure):
         self.cmd = 'traceroute -n -m %d %s ' % (maxttl, self.target)
 
     def run(self):
-        print self.cmd
         fname = self.target + '.traceroute'
         outfile = open(fname, 'w')
         traceroute = subprocess.Popen(self.cmd, stdout=outfile, stderr=subprocess.PIPE, shell=True)
         _,  err = traceroute.communicate()
-        if not err:
-            print 'ok'
+        if err:
+            logger.error('Error in %s' % self.cmd)
         outfile.close()
         self.parse_file(fname)
 
@@ -125,6 +128,54 @@ class TracerouteHop(object):
 
     def __str__(self):
         return '%d: %s, %.3f %s' % (self.hop_nr, self.ip_addr, self.rtt, str(self.endpoints))
+
+
+class Monitor(object):
+    def __init__(self, config):
+        self.config = config
+        self.db = DBClient(config)
+        self.inserted_sid = self.db.get_inserted_sid_addresses()
+        logger.info('Started active monitor: %d session(s).' % len(self.inserted_sid))
+
+    def run_active_measurement(self):
+        tot = {}
+        probed_ip = {}
+        for sid, dic in self.inserted_sid.iteritems():
+            if sid not in tot.keys():
+                tot[sid] = []
+            url = dic['url']
+            ip_addrs = dic['address']
+            for ip in ip_addrs:
+                if ip not in probed_ip.keys():
+                    probed_ip[ip] = []
+                probed_ip[ip].append(sid)
+
+                if len(probed_ip[ip]) > 1:
+                    logger.debug('IP address [%s] already computed, skipping new ping/trace' % ip)
+                    c_sid = probed_ip[ip][0]
+                    logger.debug('First sid found for IP [%s] : %d' % (ip, c_sid))
+                    logger.debug('Found %d measurements. ' % len(tot[c_sid]))
+                    ping = tot[c_sid][0]['ping']
+                    logger.debug('Ping for IP [%s] : %s' % (ip, str(ping)))
+                    trace = tot[c_sid][0]['trace']
+                    logger.debug('Trace for IP [%s] : %s' % (ip, str(trace)))
+                else:
+                    c_sid = sid
+                    ping = Ping(ip)
+                    trace = Traceroute(ip)
+                    logger.info('Running: %s ' % ping.get_cmd())
+                    ping.run()
+                    logger.info('Running: %s ' % trace.get_cmd())
+                    trace.run()
+
+                logger.debug('current %d, inserted from %d' % (sid, c_sid))
+                tot[sid].append({'url': url, 'ip': ip, 'ping': ping.get_result(), 'trace': trace.get_result()})
+                probed_ip[ip].append(sid)
+                logger.info('Computed Active Measurement for %s in session %d' % (ip, sid))
+
+        self.db.insert_active_measurement(tot)
+        logger.info('ping and traceroute saved into db.')
+
 
 if __name__ == '__main__':
     t = '8.8.8.8'
